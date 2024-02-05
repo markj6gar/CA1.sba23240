327e51745bf8d7ed4e0581c883a4b68ecd94c6c1
markj6gar <mark.j6gar@gmail.com>
2024-02-05 04:14:13 +0000

    Initial commit

---
+++ src/ca1/sba23240/CA1Sba23240.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1.sba23240;
+
+/**
+ *
+ * @author Mark
+ */
+public class CA1Sba23240 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
